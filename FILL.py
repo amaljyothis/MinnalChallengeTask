b467a82406fe3809d3c3b33db65a269ce6867028
Amal Jyothi S <amaljyothi940@gmail.com>
2022-02-17 20:49:04 +0530

    FILL FILE ADDED

---
+++ FILL.py
@@ -0,0 +1,10 @@
+print("FILL IN THE BLANKS:")
+z="MY NAME IS _____ AND I AM COMING FROM _____ AND I AM STUDYING _____ AT _____"
+print(z)
+n=input("name:")
+p=input("place:")
+d=input("course:")
+c=input("college:")
+z1="MY NAME IS {0} AND I AM COMING FROM {1} AND I AM STUDYING {2} AT {3}"
+print("YOUR ANSWER:")
+print(z1.format(n,p,d,c))
